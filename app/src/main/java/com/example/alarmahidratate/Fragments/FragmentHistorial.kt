d450e771183e9afa3bdfc14dd080b812f6cd9a5e
Sonia Villeda <sonia08_m@hotmail.com>
2019-04-23 15:50:42 -0600

    Implementación de funcionalidad Eliminar consumo en el RecyclerView

--- app/src/main/java/com/example/alarmahidratate/Fragments/FragmentHistorial.kt
+++ app/src/main/java/com/example/alarmahidratate/Fragments/FragmentHistorial.kt
@@ -1,5 +1,6 @@
 package com.example.alarmahidratate.Fragments
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,15 +25,6 @@ import com.google.firebase.database.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [FragmentHistorial.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [FragmentHistorial.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class FragmentHistorial : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
@@ -39,6 +32,7 @@ class FragmentHistorial : Fragment() {
 
     // Variables necesarias para el Fragment
     private val historialList: MutableList<Historial> = mutableListOf()
+    private val keysList: MutableList<String> = mutableListOf()
     private lateinit var adapter: AdaptadorHistorial
     lateinit var ref: DatabaseReference
 
@@ -63,7 +57,7 @@ class FragmentHistorial : Fragment() {
         val layoutManager =  LinearLayoutManager(context)
 
         // Creamos la referencia a la BD filtrando con el id del Usuario en curso
-        val ref = FirebaseDatabase.getInstance().getReference("/historial/${Datos.idUsuarioFB}").orderByChild("fecha").equalTo("21 Abril 2019")
+        val ref = FirebaseDatabase.getInstance().getReference("/historial/${Datos.idUsuarioFB}").orderByChild("fecha").equalTo("22 Abril 2019")
         // Creamos el evento para leer desde Firebase
         ref.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(p0: DatabaseError) {
@@ -71,12 +65,17 @@ class FragmentHistorial : Fragment() {
             }
             // Si se cargan los datos
             override fun onDataChange(p0: DataSnapshot) {
+                // Limpiamos las listas donde se almacean los datos del RecyclerView y las Keys
+                keysList.clear()
+                historialList.clear()
                 // Realizamos un ciclo for para poder recorrer todos los subnodos o nodos hijos
                 // que dieron como resultado según la referencia hecha al inicio (filtrado)
                 for (h in p0.children){
                     // En la siguiente variable almacenaremos el objeto de tipo Historial que
                     // trae todos los datos de Firebase y después se agregan en la Lista Mutable creada anteriormente
                     val historial = h.getValue(Historial::class.java)
+                    keysList.add(h.key!!)
+                    //Toast.makeText(activity, h.key, Toast.LENGTH_SHORT).show()
                     if (historial != null) {
                         historialList.add(historial)
                     }
@@ -85,7 +84,8 @@ class FragmentHistorial : Fragment() {
                 // el template de nuestro RecyclerView
                 adapter = AdaptadorHistorial(historialList, object : RecyclerHistorialListener {
                     override fun onClick(historial: Historial, position: Int) {
-                        Toast.makeText(activity,"Se seleccionó", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, keysList[position], Toast.LENGTH_SHORT).show()
+                        showDialog(keysList[position])
                     }
 
                     override fun onLongClick(historial: Historial, position: Int) {
@@ -104,6 +104,50 @@ class FragmentHistorial : Fragment() {
 
         })
         return vista
+
+    }
+
+    private fun showDialog(idHistorialConsumo: String){
+        // Creamos un AlertDialog
+        val builder = AlertDialog.Builder(activity)
+
+        // Colocamos un título
+        builder.setTitle("Eliminar consumo")
+
+        // Colocamos el mensaje a desplegar en el cuerpo del AlertDialog
+        builder.setMessage("¿Está seguro de elimar este consumo?")
+
+        // Cuando se presione el botón de Eliminar
+        builder.setPositiveButton("Si") { dialog, which ->
+            // Llamamos al método para eliminar
+            eliminarConsumo(idHistorialConsumo)
+        }
+
+        // Cuando se presione el botón de NO
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.cancel()
+        }
+
+        // Creamos nuestra alerta
+        val alert = builder.create()
+        // Y la mostramos
+        alert.show()
+
+    }
+
+    // Función para eliminar el registro de consumo del usuario
+    private fun eliminarConsumo(idHistorialConsumo: String){
+        // Creamos la referencia al nodo del registro del Usuario de nuestra BD
+        val refHistorial = FirebaseDatabase.getInstance().getReference("/historial/${Datos.idUsuarioFB}")
+
+        // Hacemos referencia al subnodo del consumo en específico y lo eliminamos
+        refHistorial.child(idHistorialConsumo).removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(activity, "Consumo eliminado", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(activity, "Error al eliminar", Toast.LENGTH_SHORT).show()
+            }
 
     }
 
